43495e32c185ee7ea2b43d1c2815a95aac930dec
Rwin97 <leonerwin890@gmail.com>
2020-07-08 20:34:48 -0400

    historia2

---
+++ covidlaravel2020/app/Http/Controllers/PacienteController.php
@@ -0,0 +1,97 @@
+<?php
+
+namespace App\Http\Controllers;
+use App\Paciente;
+use Illuminate\Http\Request;
+
+class PacienteController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $data = [
+        "pacientes" => Paciente::get()   
+
+        ]; //
+        return view("pacientes.listar", $data);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        return view("pacientes.create"); //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $input = $request->all();
+        $paciente=Paciente::create([
+            "nombre"   => $input["nombre"],
+            "apellido"=> $input["apellido"],
+            "CI"     => $input["ci"],
+            "correo"     => $input["correo"],
+            "Celular"     => $input["celular"],
+            
+        ]);
+        return view("pacientes.listar")->withPaciente($paciente); // 
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
+    }
+}
